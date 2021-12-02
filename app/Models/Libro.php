10687cca1bdcefdc894ce60d61381df9fc0efe7b
Braian Rico <david.rico@iudigital.edu.co>
2021-12-02 00:57:31 -0500

    Creación de módelo libro y configuración de su ruta y su controller

--- app/Models/Libro.php
+++ app/Models/Libro.php
@@ -1,10 +1,12 @@
-<?php   
+<?php
+
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Libro extends Model{
-    protected $table = "libro";
+class Libro extends Model
+{
+    protected $table = "libros";
 
     // protected $fillable = [];
 
